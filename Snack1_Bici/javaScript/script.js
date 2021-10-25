b592ca8ec0f57a623422aa4f645b37c6094deb37
andre-cyb <andrea.vg.malfatti@gmail.com>
2021-10-25 15:36:23 +0200

    creazione snack e inizio es. bici

---
+++ Snack1_Bici/javaScript/script.js
@@ -0,0 +1,64 @@
+/**
+ * Array con oggetti
+ * -gli oggetti sono le bici che contengono due key: nome, peso.
+ * -si devono comparare le bici per peso, la più leggera dovrà poi essere stampata
+ * -UTILIZZARE DESTRUCTURING e TEMPLATE LITERAL
+ */
+
+
+
+
+"use strict";
+
+
+let arrayBici =
+    [
+        {
+            nome: "Bianchi",
+            peso: 8,
+        },
+        {
+            nome: "Colnago",
+            peso: 4,
+        },
+        {
+            nome: "Boolean",
+            peso: 10,
+        },
+        {
+            nome: "B-twin",
+            peso: 6,
+        }
+    ];
+
+
+
+let arrayPeso = [];
+
+for (let i = 0; i < arrayBici.length; i++) {
+
+    let bici = arrayBici[i];
+    console.log(bici);
+
+    const { nome, peso } = arrayBici[i];
+    console.log(nome, peso);
+
+    arrayPeso.push(peso);
+
+}
+
+
+console.log(arrayPeso);
+//arrayPeso.sort();
+//console.log(arrayPeso.sort());
+
+arrayPeso.sort(function (a, b) {
+    return a - b;
+});
+console.log(arrayPeso);
+
+for (let j = 0; j < arrayPeso.length; j++) {
+    let biciPeso = arrayPeso[j];
+    console.log(biciPeso);
+
+}
